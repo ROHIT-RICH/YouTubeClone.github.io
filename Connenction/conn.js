56e4aff7dc433f5c5673a3f833f57529ef052057
Rohit Vaishya <168424765+ROHIT-RICH@users.noreply.github.com>
2025-02-07 23:11:29 +0530

    Update conn.js

--- Connenction/conn.js
+++ Connenction/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/youtubeBackend') //ADD YOUR CONNECTION
+.connect('mongodb+srv://rvaishya69:Internshala@cluster0.t7dof.mongodb.net/youtubeBackend') //ADD YOUR CONNECTION
 .then(()=> console.log('DataBase Connected')).catch(err => {
     console.log(err)
 });
